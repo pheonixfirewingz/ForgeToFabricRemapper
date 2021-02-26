da4fe743dcfc004b366f08c8e52cfa17d012d5f9
OliviaTheVampire <sindrefagerheim53@gmail.com>
2021-02-27 00:50:40 +0100

    more work on mappings stuff

--- src/main/java/cf/phoenixfirewingz/remapper/Main.java
+++ src/main/java/cf/phoenixfirewingz/remapper/Main.java
@@ -5,9 +5,8 @@ import cf.phoenixfirewingz.remapper.utils.FileHandler;
 import cf.phoenixfirewingz.remapper.utils.JsonData;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Main
 {
@@ -15,25 +14,52 @@ public class Main
 	public static Constansts constansts;
 	public static final Config config = JsonData.readJsonConfig();
 
-	private static String TINY_MAPPINGS_FILE = String.format("https://maven.fabricmc.net/net/fabricmc/yarn/%s+build.%s/yarn-%s+build.%s-tiny.gz", config.version, config.yarnVersion, config.version, config.yarnVersion);
-	public static String TINY_MAPPINGS_CONVERTED;
+	private static final String CSV_MAPPINGS =
+			String.format("https://files.minecraftforge.net/maven/de/oceanlabs/mcp/mcp_%s/%s/mcp_%s-%s.zip", config.mcpMappingsChannel, config.mcpMappings, config.mcpMappingsChannel, config.mcpMappings);
+	private static final String YARN = String.format("https://maven.fabricmc.net/net/fabricmc/yarn/%s+build.%s/yarn-%s+build.%s.jar", config.version, config.yarnVersion, config.version, config.yarnVersion);
 
 	public static void main(String[] args)
 	{
+		File cacheDir = new File(System.getProperty("user.dir") + "\\resource\\cache");
 		System.out.println("getting lastest ForgeToFabricRemapper mapping....\n");
 		constansts = new Constansts(System.getProperty("user.dir") + "\\mappings.json");
 		try {
-			try {
-				FileHandler.decompressGzip(TINY_MAPPINGS_FILE, Paths.get(System.getProperty("user.dir") + "\\resource\\tiny_mappings_" + config.version + ".txt"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			TINY_MAPPINGS_CONVERTED = FileHandler.read(true, System.getProperty("user.dir") + "\\resource\\tiny_mappings" + config.version + ".txt");
+			FileHandler.Mappings srg = FileHandler.readTsrg(
+					new Scanner(FileHandler.download(CSV_MAPPINGS, new File(System.getProperty("user.dir") + "\\resource\\cache"))),
+					FileHandler.readCsv(new Scanner(Objects.requireNonNull(FileHandler.extract(
+							FileHandler.download(CSV_MAPPINGS, new File(System.getProperty("user.dir") + "\\resource\\cache")),
+							"fields.csv",
+							new File(System.getProperty("user.dir") + "\\resource\\cache")
+					)))),
+					FileHandler.readCsv(new Scanner(Objects.requireNonNull(FileHandler.extract(
+							FileHandler.download(CSV_MAPPINGS, new File(System.getProperty("user.dir") + "\\resource\\cache")),
+							"methods.csv",
+							new File(System.getProperty("user.dir") + "\\resource\\cache")
+					))))
+			);
+
+			FileHandler.Mappings yarn = FileHandler.readYarnV1(
+					new Scanner(Objects.requireNonNull(FileHandler.extract(FileHandler.download(YARN, new File(System.getProperty("user.dir") + "\\resource\\cache")), "mappings/mappings.tiny",
+							new File(System.getProperty("user.dir") + "\\resource\\cache")
+					))),
+					"official", "named"
+			);
+
+			FileHandler.Mappings mappings = srg.invert().chain(yarn, false);
+
+
+			mappings.writeDebugMapping(new File(System.getProperty("user.dir") + "\\resource\\cache\\mappings"), new File(cacheDir, "chained_mapping.json"));
+			srg.writeDebugMapping(new File(System.getProperty("user.dir") + "\\resource\\cache\\srg"), new File(cacheDir, "mcp.json"));
+			yarn.writeDebugMapping(new File(System.getProperty("user.dir") + "\\resource\\cache\\yarn"), new File(cacheDir, "yarn.json"));
+
+			mappings.methods.forEach((s, s2) -> {
+				System.out.println("1:" + s + " 2:" + s2);
+			});
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-		System.out.println("start remapping classes.......\n");
-		readDir(new File(System.getProperty("user.dir") + "\\resource\\old"));
+//		System.out.println("start remapping classes.......\n");
+//		readDir(new File(System.getProperty("user.dir") + "\\resource\\old"));
 	}
 
 	public static void readDir(File in)
--- src/main/java/cf/phoenixfirewingz/remapper/common/Config.java
+++ src/main/java/cf/phoenixfirewingz/remapper/common/Config.java
@@ -6,4 +6,6 @@ public class Config
 	public boolean shouldMapToFabric;
 	public String version;
 	public String yarnVersion;
+	public String mcpMappings;
+	public String mcpMappingsChannel;
 }
